67a258fcd09a55ed98d684d0c949f26704529bae
Alejandro López Martínez <alejandrolm14@gmail.com>
2022-02-21 23:25:54 -0600

    Basic Characet movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,18 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#pragma warning disable 649
 
-public class CharacterMovement : MonoBehaviour
+public abstract class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] protected float _speed = 5f;
+    [SerializeField] protected float _acceleration = 10f;
+    [SerializeField] protected float _turnSpeed = 10f;
+    [SerializeField] protected bool _controlRotation = true;    // character turns towards movement direction
+
+    [Header("Airborne")]
+    [SerializeField] protected float _gravity = -20f;       // custom gravity value
+    [SerializeField] protected float _jumpHeight = 2.25f;   // peak height of jump
+    [SerializeField] protected float _airControl = 0.1f;    // percentage of acceleration applied while airborne
+    [SerializeField] protected bool _airTurning = true;     // character can turn while airborne
+
+    [Header("Grounding")]
+    [SerializeField] protected float _groundCheckOffset = 0.1f;     // height inside character where grounding ray starts
+    [SerializeField] protected float _groundCheckDistance = 0.4f;   // distance down from offset position
+    [SerializeField] protected float _maxSlopeAngle = 40f;          // maximum climbable slope, character will slip on anything higher
+    [SerializeField] protected float _groundedFudgeTime = 0.25f;    // leeway time for players to still jump after leaving the ground
+    [SerializeField] protected LayerMask _groundMask = 1 << 0;      // mask for layers considered the ground
+
+    // public read-only properties
+    public float Speed => _speed;
+    public bool IsFudgeGrounded => Time.timeSinceLevelLoad < _lastGroundedTime + _groundedFudgeTime;
+
+    // public-read private-set properties
+    public bool IsGrounded { get; protected set; } = true;
+    public Vector3 GroundNormal { get; protected set; } = Vector3.up;
+    public Vector3 MoveInput { get; protected set; }
+    public bool HasMoveInput { get; protected set; }
+    public Vector3 LocalMoveInput { get; protected set; }
+    public Vector3 LookDirection { get; protected set; }
+    public virtual Vector3 Velocity { get; protected set; }
+    public Vector3 SurfaceVelocity { get; protected set; }
+
+    // public properties
+    public bool CanMove { get; set; } = true;
+    public float MoveSpeedMultiplier { get; set; } = 1f;
+    public float ForcedMovement { get; set; } = 0f;
+    public float TurnSpeedMultiplier { get; set; } = 1f;
+
+    // protected properties
+    protected Vector3 _groundCheckStart => transform.position + transform.up * _groundCheckOffset;
+
+    // private fields
+    protected float _lastGroundedTime;
+
+    // receives movement input and clamps it to prevent over-acceleration
+    public virtual void SetMoveInput(Vector3 input)
     {
-        
+        HasMoveInput = input.magnitude > 0.1f;
+        if (!HasMoveInput) input = Vector3.zero;
+        MoveInput = Vector3.ClampMagnitude(input, 1f);
+        // finds movement input as local direction rather than world
+        LocalMoveInput = transform.InverseTransformDirection(MoveInput);
     }
 
-    // Update is called once per frame
-    void Update()
+    // sets character look direction, flattening y-value
+    public void SetLookDirection(Vector3 direction)
     {
-        
+        if (direction.magnitude < 0.1f) return;
+        LookDirection = new Vector3(direction.x, 0f, direction.z).normalized;
+    }
+
+    // attempts a jump, will fail if not grounded
+    public void Jump()
+    {
+        if (CanMove && IsFudgeGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(2f * -_gravity * _jumpHeight);
+            Velocity = new Vector3(Velocity.x, jumpVelocity, Velocity.z);
+        }
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        // rotates character towards movement direction
+        if (_controlRotation && (IsGrounded || _airTurning))
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(LookDirection);
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * _turnSpeed * TurnSpeedMultiplier);
+            transform.rotation = rotation;
+        }
+    }
+
+    protected virtual void Update()
+    {
+        IsGrounded = CheckGrounded();
+        if (!CanMove) SetMoveInput(Vector3.zero);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = IsGrounded ? Color.green : Color.red;
+        Gizmos.DrawRay(_groundCheckStart, -transform.up * _groundCheckDistance);
+    }
+
+    protected Vector3 GetMovementAcceleration(Vector3 movement, bool flattenY = true)
+    {
+        // calculates desirection movement velocity
+        Vector3 targetVelocity = movement * (_speed * MoveSpeedMultiplier);
+        // adds velocity of surface under character, if character is stationary
+        targetVelocity += SurfaceVelocity * (1f - Mathf.Abs(MoveInput.magnitude));
+        // calculates acceleration required to reach desired velocity and applies air control if not grounded
+        Vector3 velocityDiff = targetVelocity - Velocity;
+        if(flattenY) velocityDiff.y = 0f;
+        float control = IsGrounded ? 1f : _airControl;
+        Vector3 acceleration = velocityDiff * (_acceleration * control);
+        // zeros acceleration if airborne and not trying to move (allows for nice jumping arcs)
+        if (!IsGrounded && !HasMoveInput) acceleration = Vector3.zero;
+        // add gravity
+        acceleration += GroundNormal * _gravity;
+        return acceleration;
+    }
+
+    protected abstract bool CheckGrounded();
+
+    // checks if character is grounded based on floor normal
+    protected bool ResolveGrounded(bool hit, Vector3 normal)
+    {
+        // set default ground direction to up
+        GroundNormal = Vector3.up;
+        // if ground wasn't hit, character is not grounded
+        if (!hit) return false;
+
+        // test angle between character up and ground, angles above _maxSlopeAngle are invalid
+        bool angleValid = Vector3.Angle(transform.up, normal) < _maxSlopeAngle;
+        if (angleValid)
+        {
+            // record last time character was grounded and set correct floor normal direction
+            _lastGroundedTime = Time.timeSinceLevelLoad;
+            GroundNormal = normal;
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using  System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : CharacterMovement
+{
+    [Header("Top Down")] 
+    [SerializeField] private bool _topDownMovement = false;
+
+    public override Vector3 Velocity { get => _rigidbody.velocity ; protected set => _rigidbody.velocity = value; }
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        _rigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
+        _rigidbody.gravityScale = 0f;
+
+        LookDirection = Vector3.right;
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        Vector3 input = MoveInput;
+        if (ForcedMovement > 0f) input = Vector3.right * ForcedMovement;
+        Vector3 forward = Vector3.right * input.x;
+        if (_topDownMovement) forward = new Vector3(MoveInput.x, MoveInput.z, 0f);
+        _rigidbody.AddForce(GetMovementAcceleration(forward, !_topDownMovement));
+    }
+    
+    protected override bool CheckGrounded()
+    {
+        // ignore ground checks if top-down
+        if (_topDownMovement) return true;
+        // configure layer mask for 2D raycast
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.SetLayerMask(_groundMask);
+        // raycast to find ground
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+        Physics2D.Raycast(_groundCheckStart, -transform.up, filter, hits, _groundCheckDistance);
+        // gets velocity of surface underneath character if applicable
+        if (hits[0] && hits[0].rigidbody != null) SurfaceVelocity = hits[0].rigidbody.velocity;
+        else SurfaceVelocity = Vector3.zero;
+        // sends hit info for grounding resolution
+        return ResolveGrounded(hits[0], hits[0].normal);
+    }
+}
