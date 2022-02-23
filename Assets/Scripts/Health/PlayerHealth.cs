b4375ce9a6c0b8d1f9a3fac2c5c2057d97071a2b
Natalia <natguev01@gmail.com>
2022-02-22 23:24:12 -0600

    Health bar

---
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] private float _maxHealth = 100f;
+    [SerializeField] private float _curHealth = 100f;
+
+    public float Percentage => _curHealth / _maxHealth;
+    public float Current => _curHealth;
+
+    public UnityEvent<float> OnDamaged;
+    
+    public void DealDamage(float damageValue)
+    {
+        _curHealth = Mathf.Clamp(_curHealth - damageValue, 0f, _maxHealth);
+        OnDamaged.Invoke(Percentage);
+    }
+
+    //debugging
+    public void OnDebug()
+    {
+        DealDamage(Random.Range(5, 15));
+    }
+
+
+}
