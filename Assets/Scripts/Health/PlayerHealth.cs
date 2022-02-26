4b42cd83b65aa763263886a53e39a18ad73536af
Alex Moreno <morenolozalex98@gmail.com>
2022-02-25 19:26:34 -0600

    first attempt to death anim

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -9,21 +9,46 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float _maxHealth = 100f;
     [SerializeField] private float _curHealth = 10f;
 
+    [SerializeField] private float _timeBetweenDamage = 0.25f;
+    [SerializeField] private Sprite _playerDeath;
+    
+    private SpriteRenderer _characterRenderer;
+
     public float Percentage => _curHealth / _maxHealth;
     public float Current => _curHealth;
 
     public UnityEvent<float> OnDamaged;
-    
+
+    private void Start()
+    {
+        _characterRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void DealDamage(float damageValue)
     {
         _curHealth = Mathf.Clamp(_curHealth - damageValue, 0f, _maxHealth);
         OnDamaged.Invoke(Percentage);
+
+        if (_curHealth <= 0) _characterRenderer.sprite = _playerDeath;
     }
 
     public void GetHealth(float healingValue)
     {
         _curHealth = Mathf.Clamp(_curHealth + healingValue, 0f, _maxHealth);
         OnDamaged.Invoke(Percentage);
+    }
+
+    private IEnumerator FlashOnDamageCoroutine()
+    {
+        Color originalColor = _characterRenderer.color;
+        _characterRenderer.color = Color.red;
+        yield return new WaitForSeconds(_timeBetweenDamage);
+        _characterRenderer.color = originalColor;
+    }
+
+    public void FlashOnDamage()
+    {
+        StartCoroutine(FlashOnDamageCoroutine());
     }
 
     //debugging
