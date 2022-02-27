0594e30660ac4f6d214a77b451da4f2ecaef31fd
Natalia <natguev01@gmail.com>
2022-02-26 23:10:33 -0600

    Warning Animator is not playing animator controller

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -8,21 +8,28 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float attackRange;
     [SerializeField] private GameObject player;
     private ShootingController shootingController;
+    private Animator _characterAnimator;
+
+    private void Awake()
+    {
+        _characterAnimator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         shootingController = GetComponent<ShootingController>();
+        
     }
 
     private void Update()
     {
-       
         if (Vector2.Distance(transform.position, player.transform.position) <= attackRange)
         {
             //Attack 
             shootingController.canShoot = true;
             shootingController.Shoot();
+            _characterAnimator.SetBool("IsAttacking", true);
         }
         else
         {
